bace73a08c79ceace177f20a65e908bacb1f0403
liuqixing <liuqixing@dekuan.org>
2023-11-23 13:37:15 +0800

    upgraded exports

--- src/index.ts
+++ src/index.ts
@@ -1,99 +1,58 @@
 /**
  * 	@export constants
  */
-import { resultErrors } from "./constants/ResultErrors";
-export { resultErrors };
+export * from "./constants/ResultErrors";
 
 
 /**
  * 	@export interfaces
  */
-import { IWeb3StoreService } from "./interfaces/IWeb3StoreService";
-export { IWeb3StoreService };
+export * from "./interfaces/IWeb3StoreService";
 
 
 /**
  * 	@export entities
  */
-import { ERefDataTypes } from "./models/ERefDataTypes";
-export { ERefDataTypes }
+export * from "./models/ERefDataTypes";
 
-
-import { commentSchema, CommentModel } from "./entities/CommentEntity";
-import type { CommentType, CommentListResult } from "./entities/CommentEntity";
-export { commentSchema, CommentModel };
-export { CommentType, CommentListResult };
-
-
-import { contactSchema, ContactModel } from "./entities/ContactEntity";
-import type { ContactType, ContactListResult } from "./entities/ContactEntity";
-export { contactSchema, ContactModel };
-export { ContactType, ContactListResult };
-
-
-import { favoriteSchema, FavoriteModel } from "./entities/FavoriteEntity";
-import type { FavoriteType, FavoriteListResult } from "./entities/FavoriteEntity";
-export { favoriteSchema, FavoriteModel };
-export { FavoriteType, FavoriteListResult };
-
-
-import { followerSchema, FollowerModel } from "./entities/FollowerEntity";
-import type { FollowerType, FollowerListResult } from "./entities/FollowerEntity";
-export { followerSchema, FollowerModel };
-export { FollowerType, FollowerListResult };
-
-
-import { likeSchema, LikeModel } from "./entities/LikeEntity";
-import type { LikeType, LikeListResult } from "./entities/LikeEntity";
-export { likeSchema, LikeModel };
-export { LikeType, LikeListResult };
-
-
-import { postSchema, PostModel } from "./entities/PostEntity";
-import type { PostType, PostListResult } from "./entities/PostEntity";
-export { postSchema, PostModel };
-export { PostType, PostListResult };
-
-
-import { profileSchema, ProfileModel } from "./entities/ProfileEntity";
-import type { ProfileType, ProfileListResult } from "./entities/ProfileEntity";
-export { profileSchema, ProfileModel };
-export { ProfileType, ProfileListResult };
-
+export * from "./entities/CommentEntity";
+export * from "./entities/ContactEntity";
+export * from "./entities/FavoriteEntity";
+export * from "./entities/FollowerEntity";
+export * from "./entities/LikeEntity";
+export * from "./entities/PostEntity";
+export * from "./entities/ProfileEntity";
 
 
 /**
  * 	@export services
  */
-import { BaseService } from "./services/BaseService";
-import { CommentService } from "./services/CommentService";
-import { ContactService } from "./services/ContactService";
-import { FavoriteService } from "./services/FavoriteService";
-import { FollowerService } from "./services/FollowerService";
-import { LikeService } from "./services/LikeService";
-import { PostService } from "./services/PostService";
-import { ProfileService } from "./services/ProfileService";
-export { BaseService, CommentService, ContactService, FavoriteService, FollowerService, LikeService, PostService, ProfileService };
+export * from "./services/BaseService";
+export * from "./services/CommentService";
+export * from "./services/ContactService";
+export * from "./services/FavoriteService";
+export * from "./services/FollowerService";
+export * from "./services/LikeService";
+export * from "./services/PostService";
+export * from "./services/ProfileService";
+export * from "./services/PortalService";
 
 
 /**
  * 	@export utils
  */
-import { QueryUtil } from "./utils/QueryUtil";
-import { SchemaUtil } from "./utils/SchemaUtil";
-import { ServiceUtil } from "./utils/ServiceUtil";
-export { QueryUtil, SchemaUtil, ServiceUtil };
+export * from "./utils/QueryUtil";
+export * from "./utils/SchemaUtil";
+export * from "./utils/ServiceUtil";
 
 
 /**
  * 	@export configurations
  */
-import { getDatabaseUrl, setDatabaseUrl, getDatabaseOptions, setDatabaseOptions } from "./config";
-export { getDatabaseUrl, setDatabaseUrl, getDatabaseOptions, setDatabaseOptions };
+export * from "./config";
 
 /**
  * 	@export DatabaseConnection
  */
-import { DatabaseConnection } from "./connections/DatabaseConnection";
-export { DatabaseConnection };
+export * from "./connections/DatabaseConnection";
 
